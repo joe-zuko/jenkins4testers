71efbf6e636f122733c4fe670f4eaad0bf61b359
joe-zuko <joe@taekwonwikia.com.br>
2021-02-22 16:56:14 -0300

    ajustando alpine

--- build/alpine.sh
+++ build/alpine.sh
@@ -1,5 +1,5 @@
-echo "http://dl-4.alpinelinux.org/alpine/v3.9/main" >> /etc/apk/repositories && \
-echo "http://dl-4.alpinelinux.org/alpine/v3.9/community" >> /etc/apk/repositories
+echo "http://dl-4.alpinelinux.org/alpine/v3.13/main" >> /etc/apk/repositories && \
+echo "http://dl-4.alpinelinux.org/alpine/v3.13/community" >> /etc/apk/repositories
 
 apk update && \
 	apk add build-base \
