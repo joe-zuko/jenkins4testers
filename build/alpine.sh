7840f83689c298baed9f429a6a1474b7dd13b06e
joe-zuko <joe@taekwonwikia.com.br>
2021-02-23 10:08:51 -0300

    ajustando alpine4

--- build/alpine.sh
+++ build/alpine.sh
@@ -1,10 +1,3 @@
 echo "https://dl-cdn.alpinelinux.org/alpine/v3.13/main" >> /etc/apk/repositories && \
 echo "https://dl-cdn.alpinelinux.org/alpine/v3.13/community" >> /etc/apk/repositories
 
-apk update && \
-	apk add build-base \
-    libxml2-dev \
-    libxslt-dev \
-    postgresql-dev \
-    curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
-    && rm -rf /var/cache/apk/*
